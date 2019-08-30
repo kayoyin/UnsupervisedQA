6d6b17e6b6b557c885462053dfd4893d9ac24838
Kayo Yin <44864455+kayoyin@users.noreply.github.com>
2019-08-30 17:42:00 +0200

    Delete run_unmt.sh

--- unsupervisedqa/run_unmt.sh
+++
@@ -1,7 +0,0 @@
-python -m unsupervisedqa.generate_synthetic_qa_data ../unsupervised-question-answering/kayo/wikicorpus.txt unmtoutput \
-    --input_file_format "txt" \
-    --output_file_format "jsonl,squad" \
-    --translation_method unmt \
-    --use_named_entity_clozes \
-    --use_subclause_clozes \
-    --use_wh_heuristic
